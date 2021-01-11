de26184ab9537abd0ce89cbd42772ab4df2d08c8
Jeremy Langevin <langevinj832@gmail.com>
2021-01-11 13:32:14 -0500

    Customized error classes created

---
+++ expressError.js
@@ -0,0 +1,49 @@
+/**Additional error handling */
+
+class ExpressError extends Error {
+    constructor(message, status) {
+        super();
+        this.message = message;
+        this.status = status;
+    }
+}
+
+/** 404 Not Found error */
+
+class NotFoundError extends ExpressError {
+    constructor(message = "Not Found") {
+        super(message, 404);
+    }
+}
+
+/**401 Unauthorized Error */
+
+class UnauthorizedError extends ExpressError {
+    constructor(message = "Unauthorized") {
+        super(message, 401);
+    }
+}
+
+/** 400 BAD REQUEST error. */
+
+class BadRequestError extends ExpressError {
+    constructor(message = "Bad Request") {
+        super(message, 400);
+    }
+}
+
+/** 403 BAD REQUEST error. */
+
+class ForbiddenError extends ExpressError {
+    constructor(message = "Bad Request") {
+        super(message, 403);
+    }
+}
+
+module.exports = {
+    ExpressError,
+    NotFoundError,
+    UnauthorizedError,
+    BadRequestError,
+    ForbiddenError,
+};

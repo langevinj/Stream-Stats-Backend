e851707daf44df6dd801d3c95c00d824ec9b490b
Jeremy Langevin <langevinj832@gmail.com>
2021-01-11 13:24:14 -0500

    Basic routes outlined in app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+
+/** Express app for stream-stats */
+
+const express = require('express');
+const cors = require('cors');
+
+// const { NotFoundError } = require('./expressError');
+
+
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const spotifyRoutes = require('./route/spotify');
+const bandcampRoutes = require('./routes/bandcamp');
+const distrokidRoutes = require('./routes/distrokid');
+
+const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(authenticateJWT);
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/spotify", spotifyRoutes);
+app.use("/bandcamp", bandcampRoutes);
+app.use("/distrokid", distrokidRoutes);
+

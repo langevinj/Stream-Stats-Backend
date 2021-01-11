218299c26213e7e2c1eac08c793f17a427467124
Jeremy Langevin <langevinj832@gmail.com>
2021-01-11 14:33:14 -0500

    User table schema added to sql file

--- stream_stats-schema.sql
+++ stream_stats-schema.sql
@@ -1,2 +1,10 @@
 -- Database layout for stream stats app
 
+CREATE TABLE users (
+    username VARCHAR(25) PRIMARY KEY,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL
+        CHECK (position('@' IN email) > 1),
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    band_name TEXT
+);

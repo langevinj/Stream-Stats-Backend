ef48d730426a297d93910af89767ae239d55d6ae
Jeremy Langevin <langevinj832@gmail.com>
2021-01-11 13:40:32 -0500

    Database connector set up

---
+++ db.js
@@ -0,0 +1,12 @@
+/** Database setup for stream-stats */
+
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+const db = new Client({
+    connectionString: getDatabaseUri(),
+});
+
+db.connect();
+
+module.exports = db;

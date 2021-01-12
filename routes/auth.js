c0850df9457aa7c0ba810b027d0c8ee0b3fbde58
Jeremy Langevin <langevinj832@gmail.com>
2021-01-12 13:41:30 -0500

    User registration active

--- routes/auth.js
+++ routes/auth.js
@@ -37,7 +37,7 @@ router.post("/token", async function (req, res, next) {
 
 /** POST /auth/register:   { user } => { token }
  *
- * user must include { username, password, firstName, lastName, email }
+ * user must include { username, password, email }
  *
  * Returns JWT token which can be used to authenticate further requests.
  *

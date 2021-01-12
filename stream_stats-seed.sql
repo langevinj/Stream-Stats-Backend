4550fe519d86e7d84fd5ee838caa3e30a3309fb2
Jeremy Langevin <langevinj832@gmail.com>
2021-01-12 13:37:16 -0500

    Added user seed data

--- stream_stats-seed.sql
+++ stream_stats-seed.sql
@@ -1,2 +1,8 @@
 -- Seed test data for database
 
+INSERT INTO users (username, password, email, band_name, is_admin)
+VALUES('testuser',
+        'testpwd123',
+        'test@test.com',
+        'Dunce Cap',
+        FALSE);

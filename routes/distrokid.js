cf78dbef55a34ebe3525a9103cd5f05bc2da0fdd
Jeremy Langevin <langevinj832@gmail.com>
2021-01-13 11:47:06 -0500

    Setup trial rawImport route for distrokid

--- routes/distrokid.js
+++ routes/distrokid.js
@@ -1,6 +1,25 @@
+"use strict"
 
 /** Routes for Distrokid*/
+const jsonschema = require("jsonschema");
+
 const express = require("express");
+const Distrokid = require("../models/distrokid");
+
 const router = express.Router();
+
+/** POST /rawImport { page } => { response }
+ *          take raw page data from the user, parse and save it
+  */
+ 
+router.post("/rawImport", async function(req, res, next) {
+    try {
+        const response = await Distrokid.processRawImport(req.body);
+        return res.status(201).json({ response });
+
+    } catch (err) {
+        return next(err);
+    }
+});
 
 module.exports = router;

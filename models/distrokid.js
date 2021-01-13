03c9b64b297e429f452ebae1618750fa722242b3
Jeremy Langevin <langevinj832@gmail.com>
2021-01-13 12:55:24 -0500

    Successfully transferring each line of ditrokid paste to object

--- models/distrokid.js
+++ models/distrokid.js
@@ -16,8 +16,6 @@ class Distrokid {
             if(err) throw err;
             console.log('The file has been saved!');
         });
-
-        let fullData = {}
         let rawArray = []
         await fs.readFile('./distrokid.txt', function (err, data) {
             if (err) throw err;
@@ -30,6 +28,7 @@ class Distrokid {
         });
 
         let filter1;
+        let fullData = [];
         //remove the start and end of the page
         async function filterFurther(arr){
             filter1 = arr.filter(line => !line.includes("100% of team"));
@@ -42,6 +41,15 @@ class Distrokid {
 
             //remove the end of the page
             filter1.splice(endIndex, filter1.length - 1);
+
+            for (let row of filter1) {
+                //split the row by breaks
+                let t = row.split('\t');
+                let tempObj = { "reportingMonth": t[0], "saleMonth": t[1], "store": t[2], "title": t[4], "quantity": t[5], "releaseType": t[6], "paid": t[7], "saleCountry": t[8], "earnings": t[9] }
+                fullData.push(tempObj)
+            }
+
+            console.log(fullData)
         }
         
         //this whole filtering process should be cleaned up for time complexity reasons
@@ -53,10 +61,10 @@ class Distrokid {
             return line.includes('Some info about earnings');
         }
         
-        // const filter2 = filter1.map(el => el.split("  "))
-        // for (let el of filter2) {
-        //     console.log(el)
-        // }
+        
+        
+
+        
     }
 }
 

17e6f09b398836bb2bf98ddec47498f10546dbf2
Jeremy Langevin <langevinj832@gmail.com>
2021-01-13 14:00:32 -0500

    Cleaned up distrokid route

--- routes/distrokid.js
+++ routes/distrokid.js
@@ -1,22 +1,23 @@
 "use strict"
 
 /** Routes for Distrokid*/
-const jsonschema = require("jsonschema");
 
 const express = require("express");
+const { ensureCorrectUserOrAdmin } = require("../middleware/auth");
 const Distrokid = require("../models/distrokid");
 
 const router = express.Router();
 
 /** POST /rawImport { page } => { response }
- *          take raw page data from the user, parse and save it
+ *          take raw page data from the user, parse and save data to the DB
+ * 
+ * Authorization required: admin or sameuser
   */
  
-router.post("/rawImport", async function(req, res, next) {
+router.post("/rawImport", ensureCorrectUserOrAdmin, async function(req, res, next) {
     try {
         const response = await Distrokid.processRawImport(req.body);
         return res.status(201).json({ response });
-
     } catch (err) {
         return next(err);
     }

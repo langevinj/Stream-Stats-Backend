2c4eb31c51409cf3838fb9e7eac49fd64cd15343
Jeremy Langevin <langevinj832@gmail.com>
2021-01-15 11:52:07 -0500

    DB insertion of spotify credentials created

--- models/spotify.js
+++ models/spotify.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const db = require("../db");
+const bcrypt = require("bcrypt");
+
+const { BCRYPT_WORK_FACTOR } = require ("../config.js");
+
+/** Functions for Spotify */
+
+class Spotify {
+
+
+    /**Save a users Spotify credentials */
+    static async saveUserCredentials({ email, password, username }){
+        const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+        const result = await db.query(
+            `INSERT INTO spotify_credentials
+            (email, password, username)
+            VALUES ($1, $2, $3)
+            RETURNING username`, 
+            [
+                email,
+                hashedPassword,
+                username
+            ],
+        );
+        const user = result.rows[0];
+
+        return user;
+    }
+}
+
+
+module.exports = Spotify;

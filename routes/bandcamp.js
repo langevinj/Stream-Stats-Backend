eb05bcf67e8c9efa1841294172ab9ca91230f8dc
Jeremy Langevin <langevinj832@gmail.com>
2021-01-15 10:19:29 -0500

    Bandcamp route setup for importing of raw data

--- routes/bandcamp.js
+++ routes/bandcamp.js
@@ -2,6 +2,25 @@
 
 /** Routes for Bandcamp*/
 const express = require("express");
+const { ensureLoggedIn } = require("../middleware/auth");
+const Bandcamp = require("../models/bandcamp");
+
 const router = express.Router();
+
+/** POST /rawImport { page } => { response }
+ *          take raw page data from the user, parse and save data to the DB
+ *
+ * Authorization required: logged in
+  */
+
+router.post("/rawImport", ensureLoggedIn, async function (req, res, next) {
+    try {
+        const response = await Bandcamp.processRawImport(req.body);
+        return res.status(201).json({ response });
+    } catch (err) {
+        return next(err);
+    }
+});
+
 
 module.exports = router;

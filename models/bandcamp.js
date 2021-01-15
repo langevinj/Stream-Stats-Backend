8bbc483b600e652080f793f56bd6835dab3f3593
Jeremy Langevin <langevinj832@gmail.com>
2021-01-15 10:23:38 -0500

    Skeleton of bandcamp model setup

--- models/bandcamp.js
+++ models/bandcamp.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const db = require("../db");
+const fs = require("fs");
+
+const { bandcampParser } = require('../helpers/parsers');
+
+/** Functions for bandcamp. */
+
+class Bandcamp {
+
+    //parse the raw data from the user
+    static async processRawImport({ page, username }) {
+
+        /**call helper function to format all the data
+         *      returns array of objects containing each dataset
+        */
+        let formattedArray = await bandcampParser(page);
+        console.log(formattedArray);
+    }
+}
+
+
+module.exports = Bandcamp;

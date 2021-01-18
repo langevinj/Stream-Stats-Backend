89be200743d6bff72366cf8a001b9b01480e6cd5
Jeremy Langevin <langevinj832@gmail.com>
2021-01-18 10:04:34 -0500

    Insertion query written for all time data

--- models/bandcamp.js
+++ models/bandcamp.js
@@ -1,8 +1,6 @@
 "use strict";
 
 const db = require("../db");
-const fs = require("fs");
-
 const { bandcampParser } = require('../helpers/parsers');
 
 /** Functions for bandcamp. */
@@ -16,12 +14,28 @@ class Bandcamp {
          *      returns array of objects containing each dataset
         */
         let formattedArray = await bandcampParser(page, username);
-        if(formattedArray){
-            for (let el of formattedArray) {
-                console.log(el);
+        let allQueries = [];
+
+        //insertion into DB for all time data
+        for(let dataset of formattedArray){
+            try {
+                let result = db.query(
+                    `INSERT INTO bandcamp_all_time
+                    (title, plays, complete, partial, skip, username)
+                    VALUES ($1, $2, $3, $4, $5, $6)
+                    RETURNING username`, [dataset.title, dataset.plays, dataset.complete, dataset.partial, dataset.skip, dataset.username]
+                );
+                allQueries.push(result);
+            } catch (err) {
+                throw new Error("Error importing data.");
             }
         }
-        
+
+        //wait for all insertion to complete 
+        await Promise.all(allQueries);
+        let response = `The Bandcamp data has been saved!`
+        console.log(response);
+        return response;
     }
 }
 

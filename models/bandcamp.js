2388c9aaf706ab2d88e7a9bf60e077a487ba3be0
Jeremy Langevin <langevinj832@gmail.com>
2021-01-19 08:55:13 -0500

    Added get user bandcamp data method to bandcamp class

--- models/bandcamp.js
+++ models/bandcamp.js
@@ -2,6 +2,7 @@
 
 const db = require("../db");
 const { bandcampParser } = require('../helpers/parsers');
+const { NotFoundError } = require('../expressError');
 
 /** Functions for bandcamp. */
 
@@ -40,6 +41,30 @@ class Bandcamp {
         let response = `The Bandcamp data has been saved! ${count} lines processed`
         console.log(response);
         return response;
+    }
+
+    static async getUserBandcampData(username, range="alltime"){
+        const userRes = await db.query(
+            `SELECT username, is_admin as "isAdmin"
+            FROM users
+            WHERE username = $1`, [username]
+        );
+
+        const user = userRes.rows[0];
+
+        if(!user) throw new NotFoundError(`No user: ${username}`);
+
+
+        const table = range === "alltime" ? 'bandcamp_all_time' : 'bandcamp_running';
+
+        const bandcampRes = await db.query(
+            `SELECT title, plays, complete, partial, skip
+            FROM $1
+            WHERE username = $2
+            SORT BY plays DESC`, [table, username]
+        );
+
+        return bandcampRes.rows;
     }
 }
 

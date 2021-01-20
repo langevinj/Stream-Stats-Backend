cd89d777ba91e3df1306c647f0ff5508154a7468
Jeremy Langevin <langevinj832@gmail.com>
2021-01-20 11:26:01 -0500

    Removed obsolete routes from bandcamp

--- routes/bandcamp.js
+++ routes/bandcamp.js
@@ -8,35 +8,6 @@ const Bandcamp = require("../models/bandcamp");
 
 const router = express.Router();
 
-/** POST /rawAlltimeImport { page } => { response }
- *          take raw page data from the user, parse and save data to the DB
- *
- * Authorization required: correct user or admin
-  */
-
-router.post("/rawAlltimeImport/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
-    try {
-        const response = await Bandcamp.processRawImport(req.body.page, req.params.username, "alltime");
-        return res.status(201).json({ response });
-    } catch (err) {
-        return next(err);
-    }
-});
-
-/** POST /rawMonthImport { page } => { response }
- *          take raw page data for a month from the user, parse and save data to the DB
- *
- * Authorization required: correct user or admin
-  */
-
-router.post("/rawMonthImport/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
-    try {
-        const response = await Bandcamp.processRawImport(req.body.page, req.params.username, "month");
-        return res.status(201).json({ response });
-    } catch (err) {
-        return next(err);
-    }
-});
 
 /** POST /import {page} => 
  *      take a raw page of data from the user, parse, and save it to the DB

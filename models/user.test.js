3da8e0c0858deb1c5f2534b9554e4114b7618b47
Jeremy Langevin <langevinj832@gmail.com>
2021-01-29 13:15:59 -0500

    Tests for User.register added

--- models/user.test.js
+++ models/user.test.js
@@ -56,3 +56,55 @@ describe("authenticate", function () {
         }
     });
 });
+
+/*****************************register */
+
+describe("register", function() {
+    const newUser = {
+        username: "new",
+        email: "test@test.com",
+        isAdmin: false,
+        bandName: "Test Band"
+    }
+
+    test("works", async function () {
+        let user = await User.register({
+            ...newUser,
+            password: "password",
+        });
+        expect(user).toEqual(newUser);
+        const found = await db.query("SELECT * FROM users WHERE username = 'new'");
+        expect(found.rows.length).toEqual(1);
+        expect(found.rows[0].is_admin).toEqual(false);
+        expect(found.rows[0].password.startsWith("$2b$")).toEqual(true);
+    });
+
+    test("works: adds admin", async function () {
+        let user = await User.register({
+            ...newUser,
+            password: "password",
+            isAdmin: true,
+        });
+        expect(user).toEqual({ ...newUser, isAdmin: true });
+        const found = await db.query("SELECT * FROM users WHERE username = 'new'");
+        expect(found.rows.length).toEqual(1);
+        expect(found.rows[0].is_admin).toEqual(true);
+        expect(found.rows[0].password.startsWith("$2b$")).toEqual(true);
+    });
+
+    test("bad request with duplicate data", async function () {
+        try {
+            await User.register({
+                ...newUser,
+                password: "password",
+            });
+            await User.register({
+                ...newUser,
+                password: "password",
+            });
+            fail();
+        } catch (err) {
+            expect(err instanceof BadRequestError).toBeTruthy();
+        }
+    });
+});

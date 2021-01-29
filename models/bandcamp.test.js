0aa3e3a3cc66cb86b4ef281e0d2d834c0e9eacdc
Jeremy Langevin <langevinj832@gmail.com>
2021-01-29 11:04:06 -0500

    Tests written for getUserBandcampData

--- models/bandcamp.test.js
+++ models/bandcamp.test.js
@@ -19,10 +19,25 @@ afterAll(commonAfterAll);
 
 describe("getUserBandcampData", function() {
     test("works for all time data", async function() {
-        let data = await Bandcamp.getUserBandcampData("alltime", 'u1');
+        let data = await Bandcamp.getUserBandcampData('u1');
         expect(data).toEqual([
             { "title": 'song2', "plays": 200, "complete": 0, "partial": 0, "skip": 0 },
             {"title": 'song1', "plays": 100, "complete": 0, "partial": 0, "skip": 0}
         ]);
+    });
+
+    test("works for month data", async function () {
+        let data = await Bandcamp.getUserBandcampData('u2', "month");
+        expect(data).toEqual([
+            { "title": 'song3', "plays": 55, "complete": 0, "partial": 0, "skip": 0 }
+        ]);
+    });
+
+    test("it throws a NotFoundError if the user doesn't exist", async function() {
+        try {
+            await Bandcamp.getUserBandcampData("u3");
+        } catch (err) {
+            expect(err instanceof NotFoundError).toBeTruthy();
+        }
     });
 });

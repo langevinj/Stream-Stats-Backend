22c49228c4204f702e514bc3c5c5918e4534fd41
Jeremy Langevin <langevinj832@gmail.com>
2021-01-29 13:01:48 -0500

    Tests written for getUserSpotifyData

--- models/spotify.test.js
+++ models/spotify.test.js
@@ -18,3 +18,33 @@ beforeAll(commonBeforeAll);
 beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
+
+/*******************getUserSpotifyData */
+
+describe("getUserSpotifyData", function() {
+    test("works with alltime data", async function() {
+        let data = await Spotify.getUserSpotifyData('alltime', 'u1');
+        expect(data).toEqual([
+            {"title": 'song2', "streams": 2000, "listeners": 0},
+            { "title": 'song1', "streams": 1400, "listeners": 0},
+            {"title": 'song3', "streams": 50, "listeners": 0}
+        ]);
+    });
+
+    test("works with monthly data", async function () {
+        let data = await Spotify.getUserSpotifyData('month', 'u1');
+        expect(data).toEqual([
+            { "title": 'song2', "streams": 300, "listeners": 0 },
+            { "title": 'song1', "streams": 200, "listeners": 0 },
+            { "title": 'song3', "streams": 50, "listeners": 0 }
+        ]);
+    });
+
+    test("throws NotFoundError if invalid user", async function() {
+        try {
+            let data = await Spotify.getUserSpotifyData('alltime', 'u3');
+        } catch (err) {
+            expect(err instanceof NotFoundError);
+        }
+    });
+});

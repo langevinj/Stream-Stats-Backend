448dd7097b8388d92161a272d269efde301a8010
Jeremy Langevin <langevinj832@gmail.com>
2021-01-29 12:13:48 -0500

    Cleanup and Documentation

--- models/distrokid.js
+++ models/distrokid.js
@@ -2,63 +2,56 @@
 
 const jsonschema = require("jsonschema");
 const db = require("../db");
-const distrokidDataSchema = require("../schemas/distrokidData.json")
+const distrokidDataSchema = require("../schemas/distrokidData.json");
 const { BadRequestError } = require("../expressError");
-
-const { distrokidParser } = require('../helpers/parsers') 
+const { distrokidParser } = require('../helpers/parsers'); 
 
 /** Functions for distrokid. */
 
 class Distrokid {
 
-    //parse the raw data from the user
+    //Parse the raw data from the user.
     static async processRawImport(page, username) {
-        if(!page){
-            // return `No data provided for distrokid`
-            return 
-        } 
+        if(!page) return
 
-        /**call helper function to format all the data
+        /**Call helper function to format all the data.
          *      returns array of objects containing each dataset
         */
         let formattedArray = await distrokidParser(page, username);
         
         if(!formattedArray.length){
-            throw new Error("The distrokid data you provided was incorrectly formatted. Please try pasting again.")
+            throw new Error("The distrokid data you provided was incorrectly formatted. Please try copy and pasting again.")
         }
 
-        //remove outdated info from db before inputting new data
+        //Remove outdated info from db before inserting new data.
         if(formattedArray[0]){
-
-         try{
-             let res = await db.query(
+            try{
+                await db.query(
                  `DELETE FROM distrokid
                 WHERE username = $1`, [username]
-             );
-        } catch (err){
-            throw new Error("Unable to delete old data.")
-        }
-            
+                );
+            } catch (err){
+                throw new Error("Unable to delete old data.");
+            } 
         }
 
-        // await insertIntoDB(formattedArray);
+
+        const allQueries = []
+        let count = 0;
         let fails = 0;
-        // async function insertIntoDB(formattedArray){
-            let allQueries = []
-            let count = 0;
 
-        console.log(formattedArray[formattedArray.length - 1])
-            for(let dataset of formattedArray){
-                    const validator = jsonschema.validate(dataset, distrokidDataSchema);
-                    if(!validator.valid){
-                        fails++;
-                    } else {
-                        if (dataset.earnings !== undefined) {
-                            let result = db.query(
+        //Iterate through array of fomatted data and insert it into the DB.
+        for(let dataset of formattedArray){
+                const validator = jsonschema.validate(dataset, distrokidDataSchema);
+                if(!validator.valid){
+                    fails++;
+                } else {
+                    if (dataset.earnings !== undefined) {
+                        let result = db.query(
                                 `INSERT INTO distrokid
-                        (username, reporting_month, sale_month, store, title, quantity,     release_type, paid, sale_country, earnings)
-                        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-                        RETURNING username`,
+                                (username, reporting_month, sale_month, store, title, quantity, release_type, paid, sale_country, earnings)
+                                VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+                                RETURNING username`,
                                 [
                                     username,
                                     dataset.reportingMonth,
@@ -72,26 +65,25 @@ class Distrokid {
                                     parseFloat(dataset.earnings.substring(1))
                                 ],
                             );
-                            count++;
-                            allQueries.push(result);
-                        }
+                        count++;
+                        allQueries.push(result);
                     }
-            }
+                }
+        }
 
-            await Promise.all(allQueries).then(data => {
-                console.log(count)
-            })
-            // let response = `The Distrokid data has been saved! ${count} lines processed.`
-            
-        // }
+        //Wait for all the insertions to finish.
+        await Promise.all(allQueries)
+
+        //Raise any import errors.
         if (fails !== 0) {
-            throw new BadRequestError(`Error importing ${fails} distrokid lines. Please try again.`)
+            throw new BadRequestError(`Error importing ${fails} distrokid lines. Please try copy and pasting again.`)
         } else {
             return "Distrokid"
         }
-        
     }
 
+
+    //Give a username, get that user's Distrokid data.
     static async getUserDistrokidData(username, range="alltime"){
         const userRes = await db.query(
             `SELECT username, is_admin as "isAdmin"
@@ -100,11 +92,11 @@ class Distrokid {
         );
 
         const user = userRes.rows[0];
-
         if (!user) throw new NotFoundError(`No user: ${username}`);
 
         let result;
-
+        
+        //This conditional leaves room for further more specific range work.
         if(range === "alltime"){
             const distrokidRes = await db.query(
                 `SELECT title, store, SUM(quantity) AS plays, SUM(earnings) as profit
@@ -116,23 +108,9 @@ class Distrokid {
 
             result = distrokidRes.rows;
         } else {
-            //get the string for the current date
-            // let currDate = new Date();
-            // let str = currDate.toISOString().substring(0, 10);
-            
-            
+            throw new BadRequestError(`Sorry, we only support all time Distrokid data currently.`)
         }
         
-        //get a list of all the applicable stores
-        const storesRes = await db.query(
-            `SELECT store 
-            FROM distrokid
-            GROUP BY store`
-        );
-        
-        
-        
-
         return result;
     }
 }

6a907b11fab5b19a738771272d9e2ae9dda9773a
Jeremy Langevin <langevinj832@gmail.com>
2021-02-01 08:33:08 -0500

    Tests written for /users POST route

--- routes/users.test.js
+++ routes/users.test.js
@@ -20,3 +20,78 @@ beforeAll(commonBeforeAll);
 beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
+
+/**********************************POST /users */
+
+describe("POST /users", function() {
+    const data = {
+        "username": "testuser",
+        "password": "password",
+        "email": "testemail@email.com",
+        "bandName": "testband",
+        "isAdmin": false
+    }
+
+    const missingData = {
+        "username": "testuser",
+        "email": "testemail@email.com",
+        "bandName": "testband",
+        "isAdmin": false 
+    }
+
+    const badData = {
+        "username": "testuser",
+        "password": 1234,
+        "email": "testemailemail.com",
+        "bandName": "testband",
+        "isAdmin": false
+    }
+
+    test("allows an admin to add a user", async function() {
+        const resp = await request(app)
+                   .post("/users")
+                   .send(data)
+                   .set("authorization", `Bearer ${adminToken}`);
+        expect(resp.statusCode).toBe(201);
+        expect(resp.body).toEqual({
+            "user": {
+                "username": "testuser",
+                "email": "testemail@email.com",
+                "bandName": "testband",
+                "isAdmin": false
+            }, "token": expect.any(String)
+        }); 
+    });
+
+    test("allows an admin to add a user who is an admin", async function () {
+        const resp = await request(app)
+            .post("/users")
+            .send({...data, isAdmin: true})
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(resp.statusCode).toBe(201);
+        expect(resp.body).toEqual({
+            "user": {
+                "username": "testuser",
+                "email": "testemail@email.com",
+                "bandName": "testband",
+                "isAdmin": true
+            }, "token": expect.any(String)
+        });
+    });
+
+    test("bad request with missing data", async function() {
+        const resp = await request(app)
+            .post("/users")
+            .send(missingData)
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(resp.statusCode).toBe(400);
+    });
+
+    test("bad request with invalid data", async function() {
+        const resp = await request(app)
+            .post("/users")
+            .send(badData)
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(resp.statusCode).toBe(400);
+    });
+});

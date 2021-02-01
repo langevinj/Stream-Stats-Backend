ac4c1b597d196092b2936a5880e375832159f702
Jeremy Langevin <langevinj832@gmail.com>
2021-02-01 08:22:45 -0500

    Users routes test file created

---
+++ routes/users.test.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**Tests for the Distrokid routes. */
+
+const request = require("supertest");
+
+const app = require("../app");
+
+const {
+    commonBeforeAll,
+    commonBeforeEach,
+    commonAfterEach,
+    commonAfterAll,
+    u1Token,
+    u2Token,
+    adminToken
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
